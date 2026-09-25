46501d410b13ff1c6dee109660f6a802cb189c59
Leyde Duarte <maisnovus@gmail.com>
2026-09-25 14:29:49 -0300

    Remove pastas antigas

--- app/painel/jetimob/layout.tsx
+++
@@ -1,6 +0,0 @@
-// As ações da sincronização podem levar alguns segundos (Jetimob + fotos)
-export const maxDuration = 60;
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return children;
-}
